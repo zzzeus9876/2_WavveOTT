2bf566ece881079c80f08b8a2c04374e153b6b9a
은희 <withjesus211@gmail.com>
2025-12-12 18:40:22 +0900

    KIM:이것저것 정리2

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,19 @@
+import "./scss/Profile.scss";
 const Profile = () => {
   return (
-    <div>
-      <h2>회원정보</h2>
-    </div>
+    <main className="profile-wrap">
+      <div className="inner">
+        <section className="card-list">
+          <h2>회원정보</h2>
+          <div>내용</div>
+        </section>
+        <section className="card-list">
+          <h2>제목입니다</h2>
+          <div>내용</div>
+        </section>
+        <div className="">inner 안에서만 보여지면 되는 컨텐츠</div>
+      </div>
+    </main>
   );
 };
 
--- src/pages/ServiceCenter.tsx
+++ src/pages/ServiceCenter.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-
+import "./scss/ServiceCenter.scss";
 const ServiceCenter = () => {
-  return <div>고객센터</div>;
+  return (
+    <main className="service-center-wrap">
+      <div className="inner">
+        <section className="card-list">
+          <h2>고객센터</h2>
+          <div>내용</div>
+        </section>
+        <div className="">inner 안에서만 보여지면 되는 컨텐츠</div>
+      </div>
+    </main>
+  );
 };
 
 export default ServiceCenter;
