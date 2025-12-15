6cba23d43fb5d952a1b46abec76ed563036376e1
ukum <97ukum@gmail.com>
2025-12-16 01:20:27 +0900

    유겸 : mainSection수정

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import mSection from "../data/mainSection.json";
 import mStyle from "./scss/MainSection.module.scss";
 
@@ -12,44 +12,47 @@ const MainSlider = () => {
     setShowVideo(true);
     videoRef.current?.play();
   };
-  // const handlePauseVideo = () => {
-  //   setShowVideo(true);
-  //   videoRef.current?.play();
-  // };
+  const handlePauseVideo = () => {
+    setIsPlaying(false);
+    videoRef.current?.pause();
+  };
 
   return (
     <div className={mStyle.sectionBox} style={{ position: "relative" }}>
       <div className={mStyle.visualBox}>
         {!showVideo && <img src={main.main_img} alt="poster" />}
         {showVideo && (
-          <video
-            ref={videoRef}
-            className={mStyle.video}
-            src={main.main_video}
-            onPlay={() => setIsPlaying(true)}
-            onEnded={() => {
-              setIsPlaying(false);
-              setShowVideo(false);
-            }}
-            muted
-            autoPlay
-            style={{ transition: "0.1s" }}
-          />
+          <>
+            <video
+              ref={videoRef}
+              className={mStyle.video}
+              src={main.main_video}
+              onPlay={() => setIsPlaying(true)}
+              onEnded={() => {
+                setIsPlaying(false);
+                setShowVideo(false);
+              }}
+              muted
+              autoPlay
+              style={{ transition: "0.1s" }}
+            />
+            {!isPlaying && <div className={mStyle.pauseOverlay}></div>}
+          </>
         )}
         <span className={mStyle.visualBoxOverlay}></span>
       </div>
 
       <div className={mStyle.textBox}>
-        <p className={`${isPlaying ? mStyle.hideText : mStyle.textT}`}>
+        <div className={`${isPlaying ? mStyle.hideText : mStyle.textT}`}>
           <span className={mStyle.tTag}>
             <img src="/images/badge/badge-wavve-original.svg" alt="W-original" />
           </span>
           <span className={mStyle.tTitle}>
             <img src={main.main_Title} alt="title" />
           </span>
-        </p>
-        <p className={`${isPlaying ? mStyle.hideText : mStyle.textM}`}>
-          <p className="textM-t">
+        </div>
+        <div className={`${isPlaying ? mStyle.hideText : mStyle.textM}`}>
+          <p className={mStyle.textMT}>
             <span>
               <img src="images/badge/badge-19.svg" alt="" style={{ height: "30px" }} />
             </span>
@@ -58,21 +61,26 @@ const MainSlider = () => {
             <span>{main.next_episode_to_air.runtime || "2시간 3분"}</span>
           </p>
           <p className="textM-b">{main.main_desc}</p>
-        </p>
+        </div>
 
-        <p className={mStyle.btnBox}>
-          <span onClick={handleStartVideo}>
-            {isPlaying ? (
-              <img src="/images/icons/icon-play-sm.svg" alt="" />
+        <div className={mStyle.btnBox}>
+          <div className={mStyle.btnBoxT}>
+            {!isPlaying ? (
+              <span onClick={handleStartVideo}>
+                <img src="/images/icons/icon-play-sm.svg" alt="" />
+              </span>
             ) : (
-              <img src="/images/icons/icon-play-sm.svg" alt="" />
+              <span onClick={handlePauseVideo}>
+                <img src="/images/icons/icon-search-remove.svg" alt="" />
+              </span>
             )}
-          </span>
-          <span>
-            <img src="/images/icons/icon-heart-sm.svg" alt="" />
-          </span>
-        </p>
-        <p className={mStyle.watchBTN}>콘크리트마켓 보러가기</p>
+
+            <span>
+              <img src="/images/icons/icon-heart-sm.svg" alt="" />
+            </span>
+          </div>
+          <div className={mStyle.btnBoxB}>콘크리트마켓 보러가기</div>
+        </div>
       </div>
     </div>
   );
