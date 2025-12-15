de5268f5d4a07ee54fab7750a8b0eb57d03a08dd
Oh Yeongseo <wnrdmadmlane@naver.com>
2025-12-15 17:28:51 +0900

    오영서 MainNomination tsx, scss 수정

--- src/components/MainNomination.tsx
+++ src/components/MainNomination.tsx
@@ -15,22 +15,24 @@ const MainNomination = () => {
 
   return (
     <div className='main-nomination-wrap'>
-      <div className="inner">
+      <div className="nomination-inner">
         <div className='main-nomination'>
-        <div className="left-text">
-          <div className="item">
-            <h3 className='font-wave'>웨이브님을 위한 추천 콘텐츠</h3>
-            <p>지금, 찜한 콘텐츠로 <br /> 가장 완벽한 순간을 시작하세요.</p>
+          <div className="left-text">
+            <div className="item">
+              <h3 className='font-wave'>웨이브님을 위한 추천 콘텐츠</h3>
+              <p>지금, 찜한 콘텐츠로 <br /> 가장 완벽한 순간을 시작하세요.</p>
+            </div>
+            <button className='btn large secondary wFull' onClick={() => navigate("/favorite")}>
+              <p>찜 목록 보러가기</p>
+            </button>
           </div>
-          <button className='btn large secondary wFull' onClick={() => navigate("/favorite")}><p>찜 목록 보러가기</p></button>
-        </div>
-        <ul className="content-list">
-          {movies.map((m) => (
-            <li key={m.id}>
-              <img src={`https://image.tmdb.org/t/p/w500${m.poster_path}`} alt={m.title} />
-            </li>
-          ))}
-        </ul>
+          <ul className="content-list">
+            {movies.map((m) => (
+              <li key={m.id}>
+                <img src={`https://image.tmdb.org/t/p/w500${m.poster_path}`} alt={m.title} />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
