5ae7bf50d49e1be67fc3cffd4d200d99646b8f8f
jiin <amesystand@gmail.com>
2025-12-20 22:30:17 +0900

    JJI : 메인 웹예능 연말결산으로 변경

--- src/components/BroadcastList.tsx
+++ src/components/BroadcastList.tsx
@@ -1,7 +1,7 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./scss/BroadcastList.scss";
 
-interface LogoBtn{
+interface LogoBtn {
   id: number;
   name: string;
   image: string;
@@ -9,26 +9,56 @@ interface LogoBtn{
 }
 
 const broadcastBtns: LogoBtn[] = [
-  {id: 1, name: "mbc", image: "/images/badge/badge-mbc-logo.svg", url: "/home"},
-  {id: 2, name: "kbs", image: "/images/badge/badge-kbs-logo.svg", url: "/home"},
-  {id: 3, name: "jtbc", image: "/images/badge/badge-jtbc-logo.svg", url: "/home"},
-  {id: 4, name: "웹예능", image: "/images/badge/badge-웹예능-logo.svg", url: "/home"},
-  {id: 5, name: "cjenm", image: "/images/badge/badge-cjenm-logo.svg", url: "/home"},
-  {id: 6, name: "키즈", image: "/images/badge/badge-키즈-logo.svg", url: "/kids"}
-]
+  {
+    id: 1,
+    name: "연말결산",
+    image: "/images/badge/badge-2025-logo.svg",
+    url: "/home",
+  },
+  {
+    id: 2,
+    name: "mbc",
+    image: "/images/badge/badge-mbc-logo.svg",
+    url: "/home",
+  },
+  {
+    id: 3,
+    name: "kbs",
+    image: "/images/badge/badge-kbs-logo.svg",
+    url: "/home",
+  },
+  {
+    id: 4,
+    name: "jtbc",
+    image: "/images/badge/badge-jtbc-logo.svg",
+    url: "/home",
+  },
+  {
+    id: 5,
+    name: "cjenm",
+    image: "/images/badge/badge-cjenm-logo.svg",
+    url: "/home",
+  },
+  {
+    id: 6,
+    name: "키즈",
+    image: "/images/badge/badge-키즈-logo.svg",
+    url: "/kids",
+  },
+];
 
 const BroadcastList = () => {
   return (
-    <div className='broadcast-list'>
+    <div className="broadcast-list">
       {broadcastBtns.map((b) => (
-        <div className='broadcast-box' key={b.id}>
+        <div className="broadcast-box" key={b.id}>
           <Link to={b.url}>
             <img src={b.image} alt={b.name} />
           </Link>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default BroadcastList
+export default BroadcastList;
