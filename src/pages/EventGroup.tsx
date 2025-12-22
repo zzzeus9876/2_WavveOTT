434a2f73725c2ab565a490689cd0c9edb9392256
lssj9018-cyber <lssj9018@gmail.com>
2025-12-22 19:08:03 +0900

    이벤트 스타일 수정

--- src/pages/EventGroup.tsx
+++ src/pages/EventGroup.tsx
@@ -4,39 +4,37 @@ import "./scss/Event.scss";
 const EventGroup = () => {
   const location = useLocation();
 
+  // 현재 경로가 정확히 "/event" 이거나 "/event/" 일 때만 탭을 보여줌
+  // (상세 페이지 주소인 /event/123 등에서는 false가 됨)
+ const isListPage = 
+    location.pathname === "/event-group" || 
+    location.pathname === "/event-group/" || 
+    location.pathname.includes("event-winner");
+
   return (
     <main className="event-group-wrap">
       <div className="inner">
         <section>
-          <h2>EVENT</h2>
+  
           <div>
-            <ul className="event-list-tab">
-              <li>
-                <NavLink
-                  to="."
-                  end
-                  className={({ isActive }) =>
-                    // 1. 현재 경로가 기본(.)이거나
-                    // 2. 경로에 'event-winner'가 포함되어 있지 않으면서 상세 페이지인 경우 활성화
-                    isActive ||
-                    (location.pathname.includes("/event") &&
-                      !location.pathname.includes("event-winner"))
-                      ? "active"
-                      : ""
-                  }
-                >
-                  진행중인 이벤트
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="event-winner"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  당첨자 발표
-                </NavLink>
-              </li>
-            </ul>
+           {/* 탭 노출 여부 조건부 렌더링 */}
+            {isListPage && (
+            <>
+              <h2>EVENT</h2>
+              <ul className="event-list-tab">
+                <li>
+                  <NavLink to="." end className={({ isActive }) => (isActive ? "active" : "")}>
+                    진행중인 이벤트
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="event-winner" className={({ isActive }) => (isActive ? "active" : "")}>
+                    당첨자 발표
+                  </NavLink>
+                </li>
+              </ul>
+            </>
+          )}
             <Outlet />
           </div>
         </section>
