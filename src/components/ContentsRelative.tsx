fa37c5369e900daaa75d17a0520e9f2280560eba
jiin <amesystand@gmail.com>
2025-12-23 05:35:09 +0900

    JJI : 오류 수정

--- src/components/ContentsRelative.tsx
+++ src/components/ContentsRelative.tsx
@@ -1,33 +1,33 @@
-import type { Video } from '../types/movie';
+import type { Video } from "../types/movie";
 
-import './scss/ContentsRelative.scss';
+import "./scss/ContentsRelative.scss";
 
 interface RelativeProps {
-    videos: Video[];
-    backdrop: string | null;
+  videos: Video[];
+  backdrop: string | null;
 }
 
 const ContentsRelative = ({ videos }: RelativeProps) => {
-    //어떤거가 호버됐는지 체크
+  //어떤거가 호버됐는지 체크
 
-    return (
-        <div className="relative-wrap">
-            <ul className="relative-list">
-                {videos.map((v, index) => (
-                    <li key={index}>
-                        <div className="relative-card">
-                            <iframe
-                                className="epside-video"
-                                src={`https://www.youtube.com/embed/${v.key}?mute=1`}
-                                allowFullScreen
-                                title={v.type}
-                            />
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="relative-wrap">
+      <ul className="relative-list">
+        {videos.map((v, index) => (
+          <li key={index}>
+            <div className="relative-card">
+              <iframe
+                className="epside-video"
+                src={`https://www.youtube.com/embed/${v.key}?mute=1`}
+                allowFullScreen
+                title={v.type}
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ContentsRelative;
